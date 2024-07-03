55cad34893c94ca0b02611937e43da6ea6de9a4d
Konstantin (Tino) Sering <konstantin.sering@uni-tuebingen.de>
2024-07-03 14:56:33 +0200

    disables somatosensory_feedback as default

--- docs/examples/minimal_example.py
+++ docs/examples/minimal_example.py
@@ -10,7 +10,8 @@ from paule import paule, util, visualize
 util.download_pretrained_weights()
 
 #paule_model = paule.Paule(device=torch.device('cuda'), use_somatosensory_feedback=True)
-paule_model = paule.Paule(device=torch.device('cpu'), use_somatosensory_feedback=True)
+paule_model = paule.Paule(device=torch.device('cpu'),
+        use_somatosensory_feedback=False, use_speech_classifier=True)
 
 #target_acoustic = '000003-Wissenschaft.wav'
 #target_acoustic = 'froehliche_weihnachten.flac'
